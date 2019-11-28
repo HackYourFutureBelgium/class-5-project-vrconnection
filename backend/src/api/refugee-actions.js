daa1c37d2ce8968d1db2a1eabe2941ee77cd4723
berihugebre <danigebre039@gmail.com>
2019-11-28 16:25:02 +0100

    Feat(models/refugee and api/refugee-actions) building Refugee-API

---
+++ backend/src/api/refugee-actions.js
@@ -0,0 +1,84 @@
+import Refugee from '../models/refugee';
+
+const getRefugees = async (req, res) => {
+  try {
+    const refugees = await Refugee.find();
+    res.json(refugees)
+  } catch (error) {
+    res.status(400).json({message:error.message})
+  }
+};
+
+const getRefugee = async (req, res) => {
+  try {
+    const refugee = await Refugee.findById(req.params.id);
+    if (refugee === null) {
+      return res.status(400).json({ message: 'Can not find refugee' })
+    }
+    res.json(refugee)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+  
+};
+
+const createRefugee = async (req, res) => {
+  const refugee = new Refugee({
+    fullName: req.body.fullName,
+    gender: req.body.gender,
+    age: req.body.age,
+    address: req.body.address,
+    username: req.body.username
+  })
+
+  try {
+    const newRefugee = await refugee.save();
+    res.status(201).json(newRefugee);
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+};
+
+const updateRefugee = async (req, res) => {
+  try {
+    const refugee = await Refugee.findById(req.params.id);
+    
+    if (refugee === null) return res.status(400).json({ message: 'Can not update refugee' });
+    if (req.body.fullName !== null) {
+      refugee.fullName = req.body.fullName;
+    }
+    if (req.body.age !== null) {
+      refugee.age = req.body.age;
+    }
+    if (req.body.help !== null) {
+      refugee.help = req.body.help;
+    }
+    if (req.body.gender !== null) {
+      refugee.gender = req.body.gender;
+    }
+    if (req.body.address !== null) {
+      refugee.address = req.body.address;
+    }
+
+    const updatedRefugee = await refugee.save();
+    res.json(updatedRefugee);
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}; 
+
+const deleteRefuge = async (req, res) => {
+  try {
+    const refugee = await Refugee.findById(req.params.id);
+    if (refugee === null) {
+      return res.status(400).json({ message: 'There is no such refugee' })
+    }
+    await refugee.remove();
+    res.json({ message: `refugee deleted` })
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+  
+};
+
+module.exports = { getRefugees, getRefugee, createRefugee, updateRefugee, deleteRefuge };
