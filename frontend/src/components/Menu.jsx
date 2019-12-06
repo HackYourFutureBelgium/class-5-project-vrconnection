cb62114b02b589f8a81587e4ef4d76bfd23d3e61
NN <nelson.najarro@gmail.com>
2019-12-06 17:48:33 +0100

    Feature/Update add organizations and request of Help

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -7,11 +7,14 @@ const Menu = () => (
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/">Organizations</Nav.Link>
         <NavDropdown title="Refugee" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="/refugees/addHelpRequest">I need help</NavDropdown.Item>
           <NavDropdown.Item href="/refugees/profile">Profile</NavDropdown.Item>
           <NavDropdown.Item href="/refugees/signup">Sign up</NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="Volunteer" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="/volunteers/findHelpRequest">I Can Help</NavDropdown.Item>
           <NavDropdown.Item href="/volunteers/profile">Profile</NavDropdown.Item>
           <NavDropdown.Item href="/volunteers/signup">Sign up</NavDropdown.Item>
         </NavDropdown>
