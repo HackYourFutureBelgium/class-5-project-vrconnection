ab27311427c147af4d526191fd5fafd483bd1d9b
nelson.najarro <nelson.najarro@gmail.com>
2019-12-11 17:06:38 +0100

    Fix / order components to improve the readability

--- frontend/src/components/volunteerComponent/VolunteerFindHelp.jsx
+++ frontend/src/components/volunteerComponent/VolunteerFindHelp.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable react/prop-types */
 /* eslint-disable object-shorthand */
 /* eslint-disable no-nested-ternary */
 
@@ -10,13 +8,11 @@ import {
   Form,
   Row,
   Col,
-  ListGroup,
   Tab,
-  Card,
-  Badge,
 } from 'react-bootstrap';
-import dateFormat from 'dateformat';
+
 import PaginationBasic from './PaginationBasic';
+import VolunteerFindListRefugees from './VolunteerFindListRefugees'
 import API_URL from '../../api';
 import loading from '../../assets/images/load.gif';
 
@@ -89,68 +85,7 @@ const VolunteerFindHelp = () => {
             </Col>
           </Row>
         </Tab.Container>
-
-        <Tab.Container id="list-group">
-          <Row>
-            <Col sm={4}>
-              <ListGroup>
-                {
-                  refugeesInfoFilter.map((refugee, index) => {
-                    const url = `#${refugee._id}`;
-                    if (index >= (PaginationValue * (pagination - 1)) && index < (PaginationValue * pagination)) {
-                      return (
-                        <ListGroup.Item variant="secondary" action href={url} key={refugee._id}>
-                          {refugee.firstName} {refugee.lastName}
-                        </ListGroup.Item>
-                      )
-                    }
-                    return null;
-                  })
-                }
-              </ListGroup>
-            </Col>
-            <Col sm={8}>
-              <Tab.Content>
-                {
-                  refugeesInfoFilter.map((refugee) => {
-                    const url = `#${refugee._id}`;
-                    return (
-                      <Tab.Pane eventKey={url} className="information" key={refugee._id}>
-                        <Card>
-                          <Card.Header> <h4>{refugee.firstName} {refugee.lastName} </h4> Register: {dateFormat(refugee.registeredDate, 'mmmm dS, yyyy - h:MM:ss TT')}
-                          </Card.Header>
-                          <Card.Body>
-                            <Card.Text>
-                              {
-                                refugee.help.map((helpCategory) => (
-                                  <Badge pill variant="secondary" key={helpCategory}> {helpCategory} </Badge>
-                                ))
-                              }
-                            </Card.Text>
-                            <Card.Title>My personal information</Card.Title>
-                            <Card.Text>
-                              Gender: {refugee.gender} <br />
-                              Age:  {refugee.age}  <br />
-                              Country: {refugee.country}  <br />
-                              Language: {refugee.language}<br />
-                              Email: {refugee.email}<br />
-                              GSM: {refugee.phoneNumber}<br />
-                            </Card.Text>
-                            <Card.Title>In need help in </Card.Title>
-                            <Card.Text>
-                              {refugee.description}<br />
-                            </Card.Text>
-                          </Card.Body>
-                        </Card>
-                      </Tab.Pane>
-                    )
-                  })
-                }
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-        
+        <VolunteerFindListRefugees PaginationValue={PaginationValue} pagination={pagination} refugeesInfoFilter={refugeesInfoFilter} />
         <Tab.Container>
           <Row>
             <Col sm={10}>
---
+++ frontend/src/components/volunteerComponent/VolunteerFindListRefugees.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-underscore-dangle */
+
+import React from 'react';
+import dateFormat from 'dateformat';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Tab,
+  Card,
+  Badge,
+} from 'react-bootstrap';
+
+const VolunteerFindListRefugees = ({
+  PaginationValue,
+  pagination,
+  refugeesInfoFilter,
+}) => (
+
+  <Tab.Container id="list-group">
+    <Row>
+      <Col sm={4}>
+        <ListGroup>
+          {
+            refugeesInfoFilter.map((refugee, index) => {
+              const url = `#${refugee._id}`;
+              if (index >= (PaginationValue * (pagination - 1)) && index < (PaginationValue * pagination)) {
+                return (
+                  <ListGroup.Item variant="secondary" action href={url} key={refugee._id}>
+                    {refugee.firstName} {refugee.lastName}
+                  </ListGroup.Item>
+                )
+              }
+              return null;
+            })
+          }
+        </ListGroup>
+      </Col>
+      <Col sm={8}>
+        <Tab.Content>
+          {
+            refugeesInfoFilter.map((refugee) => {
+              const url = `#${refugee._id}`;
+              return (
+                <Tab.Pane eventKey={url} className="information" key={refugee._id}>
+                  <Card>
+                    <Card.Header> <h4>{refugee.firstName} {refugee.lastName} </h4> Register: {dateFormat(refugee.registeredDate, 'mmmm dS, yyyy - h:MM:ss TT')}
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        {
+                          refugee.help.map((helpCategory) => (
+                            <Badge pill variant="secondary" key={helpCategory}> {helpCategory} </Badge>
+                          ))
+                        }
+                      </Card.Text>
+                      <Card.Title>My personal information</Card.Title>
+                      <Card.Text>
+                        Gender: {refugee.gender} <br />
+                        Age:  {refugee.age}  <br />
+                        Country: {refugee.country}  <br />
+                        Language: {refugee.language}<br />
+                        Email: {refugee.email}<br />
+                        GSM: {refugee.phoneNumber}<br />
+                      </Card.Text>
+                      <Card.Title>In need help in </Card.Title>
+                      <Card.Text>
+                        {refugee.description}<br />
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Tab.Pane>
+              )
+            })
+          }
+        </Tab.Content>
+      </Col>
+    </Row>
+  </Tab.Container>
+)
+
+export default VolunteerFindListRefugees;
