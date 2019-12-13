3afc6d48bff37bdbe4c7e86a6129d7dcfd0370b9
nelson.najarro <nelson.najarro@gmail.com>
2019-12-13 20:00:16 +0100

    fix/add security to vlunteer profile accecing only by vulunteers already registed

--- frontend/src/components/volunteerComponent/VolunteerProfile.jsx
+++ frontend/src/components/volunteerComponent/VolunteerProfile.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
+import { AuthContext } from '../Auth';
+import VolunteerRegister from './VolunteerRegister';
 
-const VolunteerProfile = () => (
-  <Container className="mb-4 mt-4">
-    <h1>This is Volunteer Profile</h1>
-  </Container>
-)
+const VolunteerProfile = () => {
+  const { registeredVolunteer } = useContext(AuthContext);
+  if (registeredVolunteer !== undefined) {
+    return (
+      <Container className="mb-4 mt-4">
+        <h1>This is Volunteer Profile</h1>
+      </Container>
+    )
+  }
+  return (
+    <VolunteerRegister />
+  )
+}
 
 export default VolunteerProfile;
