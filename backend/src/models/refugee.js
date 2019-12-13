3e3973f70ff8032e1dfac7e820aa40e190b73565
nelson.najarro <nelson.najarro@gmail.com>
2019-12-13 16:53:03 +0100

    Fix/ deleting the fields password and username from backend and fronend in refugees

--- backend/src/models/refugee.js
+++ backend/src/models/refugee.js
@@ -42,18 +42,6 @@ const refugeeSchema = new mongoose.Schema({
     type: String,
     lowercase: true
   },
-  username: {
-    type: String,
-    lowercase: true,
-    required : true,
-    trim : true,
-    unique : true
-  },
-  password: {
-    type : String,
-    required : true,
-    trim : true,
-  },
   registeredDate: {
     type: Date,
     required: true,
