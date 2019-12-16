ac2820a43c80dc261f7665b82f17df86ae29a2ce
berihugebre <danigebre039@gmail.com>
2019-12-16 18:07:46 +0100

    Feature(organazation info) added

---
+++ frontend/src/data/organizationData.js
@@ -0,0 +1,34 @@
+import hackyourfutureLogo from '../assets/organizations-logo/hyflbe.jpg';
+import opendesigncourseLogo from '../assets/organizations-logo/opendesign.jpg';
+import vubLogo from '../assets/organizations-logo/vub.png'
+
+const data = [
+  {
+    name: 'Hack Your Future',
+    description: 'HackYourFuture was founded in 2015 in Amsterdam, with the aim to enable refugees to build digital skills for a career in web development, facilitate the integration of newcomers, and address the shortage of qualified workforce in the IT sector. Since 2015, HackYourFuture coding schools have also opened in Copenhagen and Malmö.',
+    logo: hackyourfutureLogo,
+    url: 'https://hackyourfuture.be/',
+    address: 'BeCentral, Cantersteen 12, 1000 Bruxelles',
+    contactNumber: '0032489785015',
+    email: 'contact@hackyourfuture.be',
+  },
+  {
+    name: 'Open Design Course',
+    description: 'Open Design Course is dedicated to refugees, asylum seekers and those who do not have access to higher education due to administrative, financial or social reasons. The course focuses on Open Design as a technological, cultural, artistic and critical practice. The course methods are based on co-creation and peer learning, putting an emphasis on new media literacy...',
+    logo: opendesigncourseLogo,
+    url: 'https://opendesigncourse.be/',
+    address: 'Jozef Kluyskensstraat 2, 9000 Gent',
+    contactNumber: '09 243 36 00',
+    email: 'info@opendesigncourse.be',
+  },
+  {
+    name: 'Vrije Universiteit Brussel',
+    description: 'n 2015, the VUB set up a programme to support the integration of refugee students. It found this necessary then, with the large influx of migrants in the EU due to numerous conflict situations in the Middle East and in the Horn of Africa. The programme was set up to give those prospective students with a refugee background a fair chance at starting or continuing their studies ...',
+    logo: vubLogo,
+    url: 'https://www.vub.be/en/welcome-student-refugees-programme#home',
+    address: 'Pleinlaan 2 1050 Brussel',
+    contactNumber: '0032489785015',
+    email: 'REFUGEE@VUB.AC.BE',
+  },
+]
+export default data;
