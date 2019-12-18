ac94bcb4223ad66b37735781e9c4f5b63e76a248
berihugebre <danigebre039@gmail.com>
2019-12-18 17:39:32 +0100

    Fix(RefugeeRegisterInfo)

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -11,14 +11,16 @@ const About = () => (
       marginBottom: '5%',
     }}
   >
-    <h1 className="bd-content">Our Project - VRConnection</h1>
+    <h1 className="bd-content">Volunteers and Refugees Connection</h1>
+    <hr />
     <p>
-      The Inspiration of Our Project is Hack Your Furute and mainly the Coaches who
-      have gaven us their time in these sevens months.
+      <strong>Volunteer and Refugee Connection(VRC) </strong>is a community based project which aims to help refugees
+      to connect them with volunteers who can help them and provide them useful information about Refugee helping organizations in Belgium.
     </p>
     <p>
-      Volunteer and Refugee Connection is a community based project which aims to help refugges
-      providing them useful information and make them connect with  volunteers that can help them.
+      The Inspiration of Our Project Is Hack Your Future community mainly the Coaches who
+      motivated us, helped us understand when we were struggling, give us their valuable time.
+      And peoples around us.
     </p>
 
     <Col xs={15} md={15}>
--- frontend/src/components/refugeeComponent/RefugeeRegisterInfo.jsx
+++ frontend/src/components/refugeeComponent/RefugeeRegisterInfo.jsx
@@ -5,23 +5,17 @@ import {
   Image,
   Alert,
 } from 'react-bootstrap';
-import logo from '../../assets/images/friends.png';
+import registeringGif from '../../assets/images/registering.gif';
 
 const RefugeeRegisterInfo = () => (
   <Container>
     <h1 className="bd-content">Do you need help?.</h1>
-
-    <p>
-      Say us in detail what you need, the volunteer want to help you.
-    </p>
-    <Col xs={6} md={6}>
-      <Image src={logo} thumbnail />
-    </Col>
-
-
     <Alert variant="warning">
-    Please fill out this form to provide your information to the volunteers.
+      Please fill out the form to provide your information to the volunteers.
     </Alert>
+    <Col xs={6} md={4}>
+      <Image src={registeringGif} rounded />
+    </Col>
 
   </Container>
 )
